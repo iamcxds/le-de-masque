90a68060cb596f7ad16f0f126aad2ebc0598d492
Poincare <iampengkeyao@gmail.com>
2026-02-01 19:56:55 +0100

    small fix

--- UI.lua
+++ UI.lua
@@ -8,6 +8,11 @@ BtnImg = {
 	r = love.graphics.newImage("assets/btn_r.png"),
 }
 
+TxtBdImg = {
+	l = love.graphics.newImage("assets/text_l.png"),
+	m = love.graphics.newImage("assets/text_m.png"),
+	r = love.graphics.newImage("assets/text_r.png"),
+}
 local cyan = { 37 / 255, 253 / 255, 233 / 255 }
 --win= {x,y,w,h,draw(),is_over(x,y):bool,click()}
 
@@ -20,23 +25,26 @@ function newWin(x, y, w, h, _draw, _click)
 	return win
 end
 
-function newBtn(x, y, w, h, text, _sc, _click)
-	local sc = _sc or 1
-	local btn_w, btn_h = BtnImg.m:getDimensions()
+local function textureDraw(t_lst, x, y, w, h, text, t_color)
+	local btn_w, btn_h = t_lst.m:getDimensions()
 	local btn_sc_w = w / btn_w
 	local btn_sc_h = h / btn_h
-	local function _draw(self)
-		love.graphics.reset()
-		local ox_l = BtnImg.l:getWidth() * btn_sc_h
-		local ox_r = BtnImg.r:getWidth() * btn_sc_h
-		love.graphics.draw(BtnImg.l, self.x - ox_l, self.y, 0, btn_sc_h)
-		love.graphics.draw(BtnImg.m, self.x, self.y, 0, btn_sc_w, btn_sc_h)
-		love.graphics.draw(BtnImg.r, self.x + self.w, self.y, 0, btn_sc_h)
-		love.graphics.setColor({ 0, 0, 0 })
-		love.graphics.printf(text, font, self.x - ox_l, self.y, (ox_l + ox_r + self.w) / sc, "center", nil, sc, sc)
-		love.graphics.reset()
-	end
+	t_color = t_color or { 0, 0, 0 }
+	love.graphics.reset()
+	local ox_l = t_lst.l:getWidth() * btn_sc_h
+	local ox_r = t_lst.r:getWidth() * btn_sc_h
+	love.graphics.draw(t_lst.l, x - ox_l, y, 0, btn_sc_h)
+	love.graphics.draw(t_lst.m, x, y, 0, btn_sc_w, btn_sc_h)
+	love.graphics.draw(t_lst.r, x + w, y, 0, btn_sc_h)
+	love.graphics.setColor(t_color)
+	love.graphics.printf(text, font, x - ox_l, y, (ox_l + ox_r + w), "center", nil )
+	love.graphics.reset()
+end
 
+local function newBtn(x, y, w, h, text, _click)
+	local function _draw(self)
+		textureDraw(BtnImg, self.x, self.y, self.w, self.h, text)
+	end
 	return newWin(x, y, w, h, _draw, _click)
 end
 
@@ -44,29 +52,17 @@ UI.MainMenu = {
 	--Title
 	-- newBtn(200, 150, 500, 80, "Le Dé Masqué", 2),
 
-	newBtn(350, 400, 100, 40, "Start", 1, function()
+	newBtn(350, 400, 100, 40, "Start", function()
 		State = "LevelMenu"
 	end),
-	newBtn(350, 500, 100, 40, "Exit", 1, function()
+	newBtn(350, 500, 100, 40, "Exit", function()
 		love.window.close()
 	end),
-	-- newWin(200, 400, 100, 50, function(self)
-	--    local img =PlayBubAnim[1]
-	--    local sc=0.5
-	--    love.graphics.setColor(1,1,1)
-	-- 	love.graphics.draw(img, self.x, self.y, nil, sc,sc, img:getWidth() / 2, img:getHeight() / 2)
-	-- end),
-	-- newWin(800, 400, 100, 50, function(self)
-	--    local img =TurnerAnim[1]
-	--    local sc=3
-	--    love.graphics.setColor(1,1,1)
-	-- 	love.graphics.draw(img, self.x, self.y, nil, sc,sc, img:getWidth() / 2, img:getHeight() / 2)
-	-- end),
 }
 
 UI.LevelMenu = {
 
-	newBtn(450, 400, 100, 40, "Back", 1, function()
+	newBtn(450, 400, 100, 40, "Back", function()
 		State = "MainMenu"
 	end),
 }
@@ -74,18 +70,10 @@ UI.LevelMenu = {
 function UI:addLevels()
 	for index, level in ipairs(Levels) do
 		index = index - 1
-		local btn = newBtn(
-			100 + 150 * (index % 4),
-			200 + 100 * math.floor(index / 4),
-			100,
-			40,
-			level.name,
-			1,
-			function()
-				GameLevel:setData(level)
-				State = "Game"
-			end
-		)
+		local btn = newBtn(100 + 150 * (index % 4), 200 + 100 * math.floor(index / 4), 100, 40, level.name, function()
+			GameLevel:setData(level)
+			State = "Game"
+		end)
 		table.insert(self.LevelMenu, btn)
 	end
 	for dir, pos in pairs(FaceMap) do
@@ -121,64 +109,58 @@ UI.Game = {
 }
 UI.Pause = {
 
-	newBtn(350, 100, 300, 40, "Resume", 1, function()
+	newBtn(250, 100, 300, 40, "Resume", function()
 		State = "Game"
 	end),
-	newBtn(350, 200, 300, 40, "Restart", 1, function()
+	newBtn(250, 200, 300, 40, "Restart", function()
 		GameLevel:restart()
 		State = "Game"
 	end),
-	newBtn(350, 300, 300, 40, "Back to Levels", 1, function()
+	newBtn(250, 300, 300, 40, "Back to Levels", function()
 		State = "LevelMenu"
 	end),
-	newBtn(350, 400, 300, 40, "Back to Menu", 1, function()
+	newBtn(250, 400, 300, 40, "Back to Menu", function()
 		State = "MainMenu"
 	end),
 }
 UI.GameOver = {
 
-	newWin(300, 50, 400, 120, function(self)
-		love.graphics.setColor(0.5, 0.5, 0.5, 0.7)
-		love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-		love.graphics.setColor({ 1, 1, 1 })
-		love.graphics.printf("You Dead! \n" .. UI.GameOver.rmk, font, self.x, self.y, self.w, "center")
+	newWin(200, 50, 400, 120, function(self)
+		textureDraw(TxtBdImg, self.x, self.y, self.w, self.h, "You Dead!\n" .. UI.GameOver.rmk, { 0.2, 0.2, 0.2 })
 	end),
-	newBtn(350, 200, 300, 40, "Undo", 1, function()
+	newBtn(250, 200, 300, 40, "Undo", function()
 		GameLevel:undo()
 		State = "Game"
 	end),
-	newBtn(350, 300, 300, 40, "Restart", 1, function()
+	newBtn(250, 300, 300, 40, "Restart", function()
 		GameLevel:restart()
 		State = "Game"
 	end),
-	newBtn(350, 400, 300, 40, "Back to Levels", 1, function()
+	newBtn(250, 400, 300, 40, "Back to Levels", function()
 		State = "LevelMenu"
 	end),
-	newBtn(350, 500, 300, 40, "Back to Menu", 1, function()
+	newBtn(250, 500, 300, 40, "Back to Menu", function()
 		State = "MainMenu"
 	end),
 }
 UI.Fini = {
 
-	newWin(300, 50, 400, 120, function(self)
-		love.graphics.setColor(0.5, 0.5, 0.5, 0.7)
-		love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
-		love.graphics.setColor(cyan)
-		love.graphics.printf("You Escaped! \n", font, self.x, self.y, self.w, "center")
+	newWin(200, 50, 400, 120, function(self)
+		textureDraw(TxtBdImg, self.x, self.y, self.w, self.h, "You Escaped!", { 0.2, 0.2, 0.2 })
 	end),
-	newBtn(350, 200, 300, 40, "Next Level", 1, function()
+	newBtn(250, 200, 300, 40, "Next Level", function()
 		if GameLevel:nextLevel() then
 			State = "Game"
 		end
 	end),
-	newBtn(350, 300, 300, 40, "Restart", 1, function()
+	newBtn(250, 300, 300, 40, "Restart", function()
 		GameLevel:restart()
 		State = "Game"
 	end),
-	newBtn(350, 400, 300, 40, "Back to Levels", 1, function()
+	newBtn(250, 400, 300, 40, "Back to Levels", function()
 		State = "LevelMenu"
 	end),
-	newBtn(350, 500, 300, 40, "Back to Menu", 1, function()
+	newBtn(250, 500, 300, 40, "Back to Menu", function()
 		State = "MainMenu"
 	end),
 }
